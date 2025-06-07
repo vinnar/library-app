f43df3b7805564f39575f11fbd70df22077725f0
Vineet Narayan <vinnar@gmail.com>
2025-06-07 07:15:59 -0400

    abstract nextAuthOptions for frontend use

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,10 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 // import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
 import { CustomPrismaAdapter } from "@/lib/customPrismaAdapter"
 
-const handler = NextAuth({
-
-    //   Use default OOB adapter
-    //   adapter: PrismaAdapter(prisma),
-    //   Using custom adapter that has additional validation baked in
+export const nextAuthOptions: NextAuthOptions = {
     adapter: CustomPrismaAdapter(prisma),
 
     providers: [
@@ -30,7 +26,37 @@ const handler = NextAuth({
         },
     },
     secret: process.env.NEXTAUTH_SECRET,
-})
+
+}
+const handler = NextAuth(nextAuthOptions)
+
+// const handler = NextAuth({
+
+//     //   Use default OOB adapter
+//     //   adapter: PrismaAdapter(prisma),
+//     //   Using custom adapter that has additional validation baked in
+//     adapter: CustomPrismaAdapter(prisma),
+
+//     providers: [
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID!,
+//             clientSecret: process.env.GITHUB_SECRET!,
+//         }),
+//     ],
+//     session: {
+//         strategy: "database",
+//     },
+//     callbacks: {
+//         async session({ session, user }) {
+//             if (session.user) {
+//                 session.user.id = parseInt(user.id)
+//                 session.user.type = user.type
+//             }
+//             return session
+//         },
+//     },
+//     secret: process.env.NEXTAUTH_SECRET,
+// })
 
 export { handler as GET, handler as POST }
 

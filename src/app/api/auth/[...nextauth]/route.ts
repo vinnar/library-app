930cabfbfd0e471a85d9b707f55a24274f6e9562
Vineet Narayan <vinnar@gmail.com>
2025-06-07 07:36:17 -0400

    force login on root landing page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,50 +13,22 @@ export const nextAuthOptions: NextAuthOptions = {
             clientSecret: process.env.GITHUB_SECRET!,
         }),
     ],
-    session: {
-        strategy: "database",
-    },
-    callbacks: {
-        async session({ session, user }) {
-            if (session.user) {
-                session.user.id = parseInt(user.id)
-                session.user.type = user.type
-            }
-            return session
-        },
-    },
-    secret: process.env.NEXTAUTH_SECRET,
+    // session: {
+    //     strategy: "database",
+    // },
+    // callbacks: {
+    //     async session({ session, user }) {
+    //         if (session.user) {
+    //             session.user.id = parseInt(user.id)
+    //             session.user.type = user.type
+    //         }
+    //         return session
+    //     },
+    // },
+    // secret: process.env.NEXTAUTH_SECRET,
 
 }
 const handler = NextAuth(nextAuthOptions)
-
-// const handler = NextAuth({
-
-//     //   Use default OOB adapter
-//     //   adapter: PrismaAdapter(prisma),
-//     //   Using custom adapter that has additional validation baked in
-//     adapter: CustomPrismaAdapter(prisma),
-
-//     providers: [
-//         GitHubProvider({
-//             clientId: process.env.GITHUB_ID!,
-//             clientSecret: process.env.GITHUB_SECRET!,
-//         }),
-//     ],
-//     session: {
-//         strategy: "database",
-//     },
-//     callbacks: {
-//         async session({ session, user }) {
-//             if (session.user) {
-//                 session.user.id = parseInt(user.id)
-//                 session.user.type = user.type
-//             }
-//             return session
-//         },
-//     },
-//     secret: process.env.NEXTAUTH_SECRET,
-// })
 
 export { handler as GET, handler as POST }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 //import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div>This is it.</div>
-  );
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+    const session = await getServerSession(nextAuthOptions);
+
+    if (!session) {
+        // redirect('/login');
+        redirect('/api/auth/signin');
+    }
+    return (
+        <div className="p-6">
+            <h1> Welcome, {session.user?.name}! </h1>
+        </div>
+    );
 }

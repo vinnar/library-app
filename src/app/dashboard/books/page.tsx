031a6820ee4333991dae62be749bf5e5cc8b5767
Vineet Narayan <vinnar@gmail.com>
2025-06-17 23:35:18 -0400

    add session check on page

--- src/app/dashboard/books/page.tsx
+++ src/app/dashboard/books/page.tsx
@@ -1,4 +1,15 @@
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation";
+
 export default async function Books() {
+
+    const session = await getServerSession(nextAuthOptions);
+
+    if (!session) {
+        redirect('/api/auth/signin')
+    }
+
     return (
         <div>
             This is the Books list page.
